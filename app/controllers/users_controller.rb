f67583080196bf4e44c052363ae371a3f0f97fd1
justrmarks <blackbirdwanderer@gmail.com>
2019-11-11 10:22:34 -0600

    authentication using jwt and User Serializers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,39 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
-  def sign_up
-    user = User.new(params)
+  def create
+    puts user_params
+    user = User.new(user_params)
+    puts user
     if user.save
-      render json: {message: "user signed up!", data: user}
+      token = encode_token(user.id)
+      render json: {message: "user signed up!", user: UserSerializer.new(user), token: token}
     else
-      render json: {message: "oh no!", data: user.errors.full_messages}
+      render json: {message: "oh no! sign up failed", data: {errors: user.errors.full_messages}}
     end
   end
 
   def index
-    users = User.all
+    users = User.all.map {|user| UserSerializer.new(user)}
     render json: {message: 'all users', data: users}
   end
 
   def profile
     user = User.find_by(id: params[:id])
-    render json: {message: "user #{user.id} name #{user.display_name}", data: user}
+    render json: {message: "user #{user.id} name #{user.display_name}", data: UserSerializer.new(user)}
   end
 
   def relationships
     user  = User.find_by(id: params[:id])
     relationships = user.relationships
     render json: {message: "user's relationships", data: relationships}
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :display_name, :age, :bio, :gender,:looking_for, :zip_code)
   end
 
 end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,5 @@
+class UserSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :id, :display_name, :age, :bio, :gender, :looking_for, :zip_code
+
+end
