c5102774203d2dc6b242c38fd98a7f9863b01076
aaron blum <blumaa@gmail.com>
2019-11-12 17:14:29 -0600

    fixed receiver/requester stuff and minor bug in user model

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,22 +3,31 @@ class RelationshipsController < ApplicationController
   def create
 
     rel = Relationship.find_or_create_by(pending: true, user_1: current_user, user_2_id: params[:user_id])
+    puts "*********current_user id**************"
+    puts current_user.id
+
+    puts "*********user 1 requester**************"
+    puts rel.user_1.id
+
+    puts "*********user 2 receiver**************"
+    puts rel.user_2.id
+
     if rel.valid?
-      render json: {user: rel.user_2, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
+      render json: {receiver: rel.user_2, requester: rel.user_1, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
     else
       render json: {message: "lone wolf!", data: relationship.errors.full_messages}
     end
   end
 
   def accept_or_decline
-    relationship = Relationship.find_by(id: params[:relationship_id])
+    rel = Relationship.find_by(id: params[:relationship_id])
     if params[:decision] == "accept"
-      relationship.pending = true
-      relationship.save
-      render json: {message: "relationship accepted", data: relationship}
+      rel.pending = false
+      rel.save
+      render json: {decision: 'accepted', requester: rel.user_1, user: rel.user_2, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
     elsif params[:decision] == "decline"
-      relationship.destroy
-      render json: {message: "relationship declined", data: relationship}
+      rel.destroy
+      render json: {decision: 'declined', requester: rel.user_1, user: rel.user_2, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
     else
       render json: {message: 'decision undefined'}
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :relationships_a, class_name: 'Relationship', foreign_key: 'user_1_id'
-  has_many :relationships_b, class_name: 'Relationship', foreign_key: 'user_1_id'
+  has_many :relationships_b, class_name: 'Relationship', foreign_key: 'user_2_id'
 
   validates :email, uniqueness: { case_insensitive: false}
 
@@ -12,12 +12,13 @@ class User < ApplicationRecord
 
   def relationships_with_messages
     payload = self.relationships.map {|rel|
-      if rel.user_1_id == self.id
-        user = rel.user_2
-      else
-        user = rel.user_1
-      end
-      {user: user, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
+      puts "*********current_user id**************"
+      puts self.id
+      puts "*********user 2**************"
+      puts rel.user_2.id
+      puts "*********user 1**************"
+      puts rel.user_1.id
+      {receiver: rel.user_2, requester: rel.user_1, relationship_id: rel.id, pending: rel.pending, messages: rel.messages, created_at: rel.created_at}
     }
 
     payload
